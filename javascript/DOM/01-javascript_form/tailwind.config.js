c2bb3be4a0c56ac2d56d646c24a559ec56c720bb
dineshdevelope <dineshindevelope@gmail.com>
2024-01-11 22:32:22 +0530

    fix:repo-structure

--- javascript/DOM/01-javascript_form/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./main.js", "./src/**/*.{html,js}"],
-
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
