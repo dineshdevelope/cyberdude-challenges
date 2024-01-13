fb2ed150efa936a752c94992807b36795a20959b
dineshdevelope <dineshindevelope@gmail.com>
2024-01-13 17:47:55 +0530

    fix

--- javascript/02-javascript-dom/07-adviceslip-api/assets/js/main.js
+++ javascript/02-javascript-dom/07-adviceslip-api/assets/js/main.js
@@ -8,29 +8,53 @@
 //Promises
 const decition = new Promise((reslove, reject) => {
   //let resign;
-
   let resign = false;
 
   // 3 sec wait
-  /* setTimeout(() => {
-    resign = true;
-  }, 3000); */
-  if (resign) {
-    reslove({ id: 1, result: "" });
-  } else {
-    reject("Bad Idea");
-  }
+  setTimeout(() => {
+    if (resign) {
+      reslove({ id: 1, result: "Good Idea" });
+    } else {
+      reject({ result: "Bad Idea" });
+    }
+  }, 3000);
 });
 
-decition
+/* decition
   .then((msg) => {
     console.log(msg);
   })
   .catch((reject) => {
-    console.log(` Error : ${reject}`);
+    console.log(reject);
   })
   .finally(() => {
     console.log(` Finally You did It`);
-  });
+  }); */
 
 //console.log(decition);
+
+// await
+/* async function getdecition() {
+  const finalResult = await decition;
+  console.log(finalResult);
+} */
+
+const blackQuoteEl = document.querySelector("blockquote");
+
+console.log(blackQuoteEl);
+
+const getdecition = async () => {
+  try {
+    const finalResult = await decition;
+
+    const msg = "King Is King !";
+    blackQuoteEl.innerText = msg + JSON.stringify(finalResult);
+
+    console.log(finalResult);
+  } catch (error) {
+    const msg = "Queen Is Queen !";
+    blackQuoteEl.innerText = msg + JSON.stringify(error.result);
+  }
+};
+
+getdecition();
